101a4f7097b182554e8011b4799f4e551b5f160d
AceInco <90135395+AceInco@users.noreply.github.com>
2026-03-07 14:54:27 +0300

    Update server.py

--- backend/server.py
+++ backend/server.py
@@ -91,6 +91,10 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
+@app.on_event("startup")
+async def startup_event():
+    asyncio.create_task(start_bot())
+
 @app.on_event("shutdown")
 async def shutdown_db_client():
     client.close()
